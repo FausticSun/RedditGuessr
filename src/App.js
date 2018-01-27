2a41ae55808e78434a1a59667a3b28da36d4deab
FausticSun <FausticSun@gmail.com>
2018-01-28 01:16:51 +0800

    Ensured at least 150 posts are retrieved

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import sampleSize from 'lodash/sampleSize';
-import { fetchRedditPosts } from './actions/RedditActions';
+import { fetchRedditPosts, completeLoading } from './actions/RedditActions';
 import { changePage } from './actions/RoutingActions';
 import LoadingScreen from './components/LoadingScreen';
 import HomeScreen from './components/HomeScreen';
 import QuestionScreen from './components/QuestionScreen';
 import ScoreScreen from './components/ScoreScreen';
+import shuffle from 'lodash/shuffle';
 
 class App extends Component {
   componentDidMount() {
-    this.props.dispatch(fetchRedditPosts());
+    this.props.dispatch(fetchRedditPosts(""));
     this.props.dispatch(changePage("LoadingScreen"));
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.isLoadingPosts && !this.props.isLoadingPosts) {
       this.props.dispatch(changePage("HomeScreen"));
+    }
+    if (this.props.posts.length > 0 && this.props.posts.length < 150) {
+      this.props.dispatch(fetchRedditPosts(this.props.posts[this.props.posts.length - 1].data.name));
+    } else if (this.props.posts.length >= 150) {
+      this.props.dispatch(completeLoading());
     }
   }
 
@@ -35,7 +41,7 @@ class App extends Component {
         break;
       case "QuestionScreen":
         page = (<QuestionScreen
-          questions={sampleSize(this.props.posts, 10)}
+          questions={shuffle(this.props.posts).slice(0,10)}
           score={this.props.score}
         />);
         break;
--- src/actions/RedditActions.js
+++ src/actions/RedditActions.js
@@ -3,13 +3,18 @@ import { RSAA } from 'redux-api-middleware'; // RSAA = '@@redux-api-middleware/R
 export const REQUEST = 'FETCH_REQUEST';
 export const SUCCESS = 'FETCH_SUCCESS';
 export const FAILURE = 'FETCH_FAILURE';
+export const LOADING_COMPLETE = 'LOADING_COMPLETE';
 
-export function fetchRedditPosts() {
+export function fetchRedditPosts(name) {
     return {
         [RSAA]: {
-            endpoint: 'http://www.reddit.com/r/popular.json?limit=100',
+            endpoint: `http://www.reddit.com/r/popular.json?limit=100&after=${name}`,
             method: 'GET',
             types: [REQUEST, SUCCESS, FAILURE]
         }
     }
+}
+
+export function completeLoading() {
+    return { type: LOADING_COMPLETE };
 }
