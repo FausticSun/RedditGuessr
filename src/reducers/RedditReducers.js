386fff461fe6b1f38b565a7e9676300c12467b20
FausticSun <FausticSun@gmail.com>
2018-01-27 21:38:10 +0800

    Filtered subreddits not in list

--- src/reducers/RedditReducers.js
+++ src/reducers/RedditReducers.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
 import { SUCCESS } from '../actions/RedditActions';
 
+const subredditSimilarity = require('../constants/SubredditSimilarity.json');
+
 function posts(state = null, action) {
   switch (action.type) {
     case SUCCESS:
-      return action.payload.data.children;
+      return action.payload.data.children.filter(post =>
+        subredditSimilarity.subreddits.find(subreddit =>
+          subreddit.name === post.data.subreddit) !== undefined);
     default:
       return state;
   }
